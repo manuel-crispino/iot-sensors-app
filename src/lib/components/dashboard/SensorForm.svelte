a807105488a3ef363547d609b5177e55d54e9044
ManuelCrispino <manuel.barman96@gmail.com>
2025-10-28 14:54:06 +0100

    SensorForm issue fixed

--- src/lib/components/dashboard/SensorForm.svelte
+++ src/lib/components/dashboard/SensorForm.svelte
@@ -21,22 +21,22 @@
   class="bg-white p-4 rounded shadow flex flex-col gap-2"
   on:submit|preventDefault={handleSubmit}
 >
-  <label for="">Nombre</label>
-  <input bind:value={sensor.nombre} class="border p-1 rounded" required/>
+  <label for="nombre">Nombre</label>
+  <input id="nombre" bind:value={sensor.nombre} class="border p-1 rounded" required/>
 
-  <label for="">Tipo</label>
-  <input bind:value={sensor.tipo} class="border p-1 rounded" required/>
+  <label for="tipo">Tipo</label>
+  <input id="tipo" bind:value={sensor.tipo} class="border p-1 rounded" required/>
 
-  <label for="">Valor</label>
-  <input type="number" step="0.01" bind:value={sensor.valor} class="border p-1 rounded" required/>
+  <label for="valor">Valor</label>
+  <input id="valor" type="number" step="0.01" bind:value={sensor.valor} class="border p-1 rounded" required/>
 
-  <label for="">Estado</label>
-  <select bind:value={sensor.estado} class="border p-1 rounded">
+  <label for="estado">Estado</label>
+  <select id="estado" bind:value={sensor.estado} class="border p-1 rounded">
     <option value={true}>Activo</option>
     <option value={false}>Inactivo</option>
   </select>
 
-  <button type="submit" class="bg-blue-500 text-white rounded p-2 mt-2 hover:bg-blue-600">
+  <button type="submit" class="bg-blue-500 cursor-pointer text-white rounded p-2 mt-2 hover:bg-blue-600">
     Guardar
   </button>
 </form>
